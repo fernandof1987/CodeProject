50e5fc6b6decdfff9987ba990f85b9baa823bc24
unknown <fernandof1987@gmail.com>
2015-11-29 02:16:59 -0200

    Layout copu-fonts

--- public/build/js/services/client.js
+++
@@ -1,4 +0,0 @@
-angular.module('app.services')
-    .service('Client', ['$resource', 'appConfig', function($resource, appConfig){
-        return $resource(appConfig.baseUrl + '/client/:id', {id: '@id'});
-    }]);
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -29,6 +29,18 @@ app.config([
            templateUrl: 'build/views/client/list.html',
            controller: 'ClientListController'
        })
+       .when('/clients/new', {
+           templateUrl: 'build/views/client/new.html',
+           controller: 'ClientNewController'
+       })
+       .when('/clients/:id/edit', {
+           templateUrl: 'build/views/client/edit.html',
+           controller: 'ClientEditController'
+       })
+       .when('/clients/:id/remove', {
+           templateUrl: 'build/views/client/remove.html',
+           controller: 'ClientRemoveController'
+       })
         .when('/home', {
             templateUrl: 'build/views/home.html',
             controller: 'HomeController'
